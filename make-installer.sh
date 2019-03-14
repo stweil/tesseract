e7c89d05927df8110e4f14ed3e613a45ef873c4d
Stefan Weil <sw@weilnetz.de>
2019-03-14 11:23:53 +0100

    Add script for building installers
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

---
+++ make-installer.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+TAG=4.1.0.$(date +%Y%m%d)
+
+git tag -a v$TAG -m "Tesseract $TAG"
+
+ARCHS="i686 x86_64 x86_64"
+
+for ARCH in $ARCHS; do
+  HOST=$ARCH-w64-mingw32
+
+  rm -rf bin/ndebug/$HOST
+  mkdir -p bin/ndebug/$HOST
+  (
+  cd bin/ndebug/$HOST
+  ../../../configure --host=$HOST --prefix=/usr/$HOST CXXFLAGS="-fno-math-errno -Wall -Wextra -Wpedantic -g -O2"
+  make install-jars install training-install winsetup prefix=$PWD/usr/$HOST
+  )
+done
