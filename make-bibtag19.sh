9d55188210347998e29e6755fbabd0a661f470ee
Stefan Weil <sw@weilnetz.de>
2019-06-23 10:01:50 +0200

    Add old build scripts for ELAG and bibtag19
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

---
+++ make-bibtag19.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#TAG=4.1.0.$(date +%Y%m%d)
+TAG=4.1.0-bibtag19
+
+git tag -a v$TAG -m "Tesseract $TAG"
+
+ARCHS="i686 x86_64 x86_64"
+
+./autogen.sh
+
+for ARCH in $ARCHS; do
+  HOST=$ARCH-w64-mingw32
+
+  rm -rf bin/ndebug/$HOST
+  mkdir -p bin/ndebug/$HOST
+  (
+  cd bin/ndebug/$HOST
+  ../../../configure --host=$HOST --prefix=/usr/$HOST CXXFLAGS="-fno-math-errno -Wall -Wextra -Wpedantic -g -O2"
+  make install-jars install training-install html winsetup prefix=$PWD/usr/$HOST
+  )
+done
