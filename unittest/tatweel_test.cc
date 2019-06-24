ec70ec587f8dc2b0157d69d74ffbbef02df0fd62
Stefan Weil <sw@weilnetz.de>
2019-06-24 12:52:06 +0200

    unittest: Fix and enable tatweel_test
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- unittest/tatweel_test.cc
+++ unittest/tatweel_test.cc
@@ -1,7 +1,9 @@
-#include "tesseract/ccutil/unicharset.h"
-#include "tesseract/dict/dawg.h"
-#include "tesseract/dict/trie.h"
-#include "util/utf8/public/unicodetext.h"
+
+#include "include_gunit.h"
+#include "dawg.h"
+#include "trie.h"
+#include "unicharset.h"
+#include "util/utf8/unicodetext.h"  // for UnicodeText
 
 namespace {
 
@@ -12,24 +14,25 @@ class TatweelTest : public ::testing::Test {
   }
 
   TatweelTest() {
-    string filename = TestDataNameToPath("ara.wordlist");
-    string wordlist;
+    std::string filename = TestDataNameToPath("ara.wordlist");
+    std::string wordlist;
+    wordlist = u8"\u0640";
     CHECK_OK(file::GetContents(filename, &wordlist, file::Defaults()));
     // Put all the unicodes in the unicharset_.
     UnicodeText text;
     text.PointToUTF8(wordlist.data(), wordlist.size());
     int num_tatweel = 0;
     for (auto it = text.begin(); it != text.end(); ++it) {
-      string utf8 = it.get_utf8_string();
-      if (utf8.find(u8"\u0640") != string::npos) ++num_tatweel;
+      std::string utf8 = it.get_utf8_string();
+      if (utf8.find(u8"\u0640") != std::string::npos) ++num_tatweel;
       unicharset_.unichar_insert(utf8.c_str());
     }
     LOG(INFO) << "Num tatweels in source data=" << num_tatweel;
     EXPECT_GT(num_tatweel, 0);
   }
 
-  string TestDataNameToPath(const string& name) {
-    return file::JoinPath(FLAGS_test_srcdir, "testdata/" + name);
+  std::string TestDataNameToPath(const std::string& name) {
+    return file::JoinPath(TESTDATA_DIR, name);
   }
   UNICHARSET unicharset_;
 };
@@ -46,7 +49,7 @@ TEST_F(TatweelTest, DictIgnoresTatweel) {
   // This test verifies that the dictionary ignores the Tatweel character.
   tesseract::Trie trie(tesseract::DAWG_TYPE_WORD, "ara", SYSTEM_DAWG_PERM,
                        unicharset_.size(), 0);
-  string filename = TestDataNameToPath("ara.wordlist");
+  std::string filename = TestDataNameToPath("ara.wordlist");
   EXPECT_TRUE(trie.read_and_add_word_list(
       filename.c_str(), unicharset_, tesseract::Trie::RRP_REVERSE_IF_HAS_RTL));
   EXPECT_EQ(0, trie.check_for_words(filename.c_str(), unicharset_, false));
@@ -55,7 +58,7 @@ TEST_F(TatweelTest, DictIgnoresTatweel) {
 TEST_F(TatweelTest, UnicharsetLoadKeepsTatweel) {
   // This test verifies that a load of an existing unicharset keeps any
   // existing tatweel for backwards compatibility.
-  string filename = TestDataNameToPath("ara.unicharset");
+  std::string filename = TestDataNameToPath("ara.unicharset");
   EXPECT_TRUE(unicharset_.load_from_file(filename.c_str()));
   int num_tatweel = 0;
   for (int i = 0; i < unicharset_.size(); ++i) {
