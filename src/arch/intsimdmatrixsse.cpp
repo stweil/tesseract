a71edc95751233ce6bccccc7eb7f5e78e965ae2b
Stefan Weil <sw@weilnetz.de>
2021-07-13 22:30:56 +0200

    Implement fast float dotproduct for SSE IntSimdMatrix
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- src/arch/intsimdmatrixsse.cpp
+++ src/arch/intsimdmatrixsse.cpp
@@ -23,7 +23,36 @@
 #  endif
 #elif defined(FAST_FLOAT)
 namespace tesseract {
-const IntSimdMatrix *IntSimdMatrix::intSimdMatrixSSE = nullptr;
+static void matrixDotVector(int dim1, int dim2, const int8_t *wi, const float *scales,
+                            const int8_t *u, float *v) {
+  const int num_out = dim1;
+  const int num_in = dim2 - 1;
+//#pragma omp simd collapse(2)
+  for (int i = 0; i < num_out; ++i) {
+    int total = 0;
+#pragma omp simd reduction(+:total)
+    for (int j = 0; j < num_in; ++j) {
+      total += wi[j] * u[j];
+    }
+    // Add in the bias and correct for integer values.
+    v[i] = (total + wi[num_in] * INT8_MAX) * scales[i];
+    wi += dim2;
+  }
+}
+
+static const IntSimdMatrix simdMatrix = {
+    matrixDotVector,
+    // Number of 32 bit outputs held in each register.
+    1,
+    // Maximum number of registers that we will use to hold outputs.
+    1,
+    // Number of 8 bit inputs in the inputs register.
+    1,
+    // Number of inputs in each weight group.
+    1
+};
+
+const IntSimdMatrix *IntSimdMatrix::intSimdMatrixSSE = &simdMatrix;
 }
 #else
 
