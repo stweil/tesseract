77cd8618ef2e2920372214e60907e7df76c72291
Ger Hobbelt <ger@hobbelt.com>
2021-07-15 15:25:29 +0200

    Place TFloat type in the tesseract namespace, same as has been done with all other tesseract defined types, to prevent collisions with thirdparty software.

--- src/ccutil/tfloat.h
+++ src/ccutil/tfloat.h
@@ -1,10 +1,14 @@
 #ifndef TESSERACT_LSTM_TFLOAT_H
 #define TESSERACT_LSTM_TFLOAT_H
 
+namespace tesseract {
+
 #ifdef FAST_FLOAT
 typedef float TFloat;
 #else
 typedef double TFloat;
 #endif
+
+}
 
 #endif
